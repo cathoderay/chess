0306894c24fcaba046c519fcb735ffc25539adfa
Ronald Kaiser <raios.catodicos@gmail.com>
2012-10-04 10:33:19 -0300

    renamings (better definition of tests names)

--- test_board.py
+++ test_board.py
@@ -25,15 +25,16 @@ class BoardTest(unittest.TestCase):
 
     def test_set_config_from_flat(self):
         flat = 'RNBQKBNRPPPPPPPP____________________p___________pppp_ppprnbqkbnr'
+        matrix = Board.to_matrix(flat)
 
         b = Board()
         b.set_config(flat=flat)
 
-        expected = Board.to_matrix(flat)
+        expected = matrix
         result = b.matrix
         self.assertEqual(expected, result)
 
-    def test_from_matrix_to_flat_string(self):
+    def test_to_flat(self):
         matrix = ['R N B Q K B N R',
                   'P P P P P P P P',
                   '_ _ _ _ _ _ _ _',
@@ -43,11 +44,12 @@ class BoardTest(unittest.TestCase):
                   'p p p p p p p p',
                   'r n b q k b n r']
         matrix = map(lambda x: x.split(' '), matrix)
+
         expected = conf.INITIAL_BOARD
         result = Board.to_flat(matrix)
         self.assertEqual(expected, result)
 
-    def test_from_flat_string_to_matrix(self):
+    def test_to_matrix(self):
         flat = "RNBQKBNRPPPPPPPP____________________p___________pppp_ppprnbqkbnr"
         matrix = ['R N B Q K B N R',
                   'P P P P P P P P',
@@ -57,11 +59,12 @@ class BoardTest(unittest.TestCase):
                   '_ _ _ _ _ _ _ _',
                   'p p p p _ p p p',
                   'r n b q k b n r']
+
         expected = map(lambda x: x.split(' '), matrix)
         result = Board.to_matrix(flat)
         self.assertEqual(expected, result)
 
-    def test_reset_board(self):
+    def test_reset(self):
         flat = "RNBQKBNRPPPPPPPP____________________pp___________pppp__pprnbqkbnr"
         some_config = Board.to_matrix(flat)
 
@@ -75,16 +78,16 @@ class BoardTest(unittest.TestCase):
         result = b.matrix
         self.assertEqual(expected, result)
 
-    def test_flat_wrong_number_of_places(self):
+    def test_invalid_number_of_places_raises_exception(self):
         flat = "rrrRRR"
         self.assertRaises(InvalidBoard, Board.validate, flat=flat)
 
-    def test_invalid_char_raises_exception(self):
+    def test_invalid_chars_raises_exception(self):
         flat = "_"*63 + "x"
         self.assertRaises(InvalidBoard, Board.validate, flat=flat)
 
     @patch('board.Board.validate')
-    def test_set_config_validates_board(self, validate):
+    def test_validation_is_done_when_set_config(self, validate):
         flat = "RNBQKBNRPPPPPPPP____________________pp___________pppp__pprnbqkbnr"
 
         b = Board()
