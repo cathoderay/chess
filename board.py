32695fbc5c987eaf8ebbb16fc98dceb95db0693c
Ronald Kaiser <raios.catodicos@gmail.com>
2012-10-03 23:34:32 -0300

    tests to translate from matrix to flat strings

--- board.py
+++ board.py
@@ -1,13 +1,18 @@
+import conf
+
+
 class Board:
     def __init__(self):
-        self.matrix = [['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
-                       ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-                       ['_', '_', '_', '_', '_', '_', '_', '_'],
-                       ['_', '_', '_', '_', '_', '_', '_', '_'],
-                       ['_', '_', '_', '_', '_', '_', '_', '_'],
-                       ['_', '_', '_', '_', '_', '_', '_', '_'],
-                       ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-                       ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']]
+        self.matrix = self.from_flat(conf.INITIAL_BOARD)
 
-        
+    def set_config(self, matrix):
+        self.matrix = matrix
 
+    def to_flat(self, matrix=None):
+        matrix = matrix or self.matrix
+        return ''.join([e for l in matrix
+                          for e in l])
+
+    def from_flat(self, flat):
+        return [list(flat[l*8:l*8+8])
+                for l in xrange(8)]
--- test_board.py
+++ test_board.py
@@ -1,5 +1,6 @@
 import unittest
 
+import conf
 from board import Board
 
 
@@ -13,10 +14,46 @@ class BoardTest(unittest.TestCase):
                   '_ _ _ _ _ _ _ _',
                   'p p p p p p p p',
                   'r n b q k b n r']
-        expected = map(lambda x: x.split(' '), matrix) 
+        expected = map(lambda x: x.split(' '), matrix)
         result = Board().matrix
         self.assertEqual(expected, result)
-        
+
+    def test_set_config(self):
+        matrix = ['R N B Q K B N R',
+                  'P P P P P P P P',
+                  '_ _ _ _ _ _ _ _',
+                  '_ _ _ _ _ _ _ _',
+                  '_ _ _ _ p _ _ _',
+                  '_ _ _ _ _ _ _ _',
+                  'p p p p _ p p p',
+                  'r n b q k b n r']
+        matrix = expected = map(lambda x: x.split(' '), matrix)
+        b = Board()
+        b.set_config(matrix)
+        result = b.matrix
+        self.assertEqual(expected, result)
+
+    def test_from_matrix_to_flat_string(self):
+        expected = conf.INITIAL_BOARD
+        b = Board()
+        result = b.to_flat()
+        self.assertEqual(expected, result)
+
+    def test_from_flat_string_to_matrix(self):
+        flat = "RNBQKBNRPPPPPPPP____________________p___________pppp_ppprnbqkbnr"
+        matrix = ['R N B Q K B N R',
+                  'P P P P P P P P',
+                  '_ _ _ _ _ _ _ _',
+                  '_ _ _ _ _ _ _ _',
+                  '_ _ _ _ p _ _ _',
+                  '_ _ _ _ _ _ _ _',
+                  'p p p p _ p p p',
+                  'r n b q k b n r']
+        expected = map(lambda x: x.split(' '), matrix)
+        b = Board()
+        result = b.from_flat(flat)
+        self.assertEqual(expected, result)
+
 
 if __name__ == '__main__':
     unittest.main()
