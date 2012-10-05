7416d188f2b7f4b8368d108098227b953ffda256
Ronald Kaiser <raios.catodicos@gmail.com>
2012-10-05 17:58:14 -0300

    more tests for pawn movements (now, for black ones)

--- tests/test_pawn.py
+++ tests/test_pawn.py
@@ -13,46 +13,34 @@ class PawnTest(unittest.TestCase):
         pawn = Pawn(Board(), square=Square(1, 1), color=conf.BLACK)
         assert init.called
 
-    def test_valid_moves_initial_position(self):
+    def test_valid_moves_initial_position_white(self):
         pawn = Pawn(Board(), square=Square(6, 3), color=conf.WHITE)
         expected = [Square(5, 3), Square(4, 3)]
         result = pawn.valid_moves()
         self.assertEqual(expected, result)
+        
+    def test_valid_moves_initial_position_black(self):
+        pawn = Pawn(Board(), square=Square(1, 3), color=conf.BLACK)
+        expected = [Square(2, 3), Square(3, 3)]
+        result = pawn.valid_moves()
+        self.assertEqual(expected, result)
 
-    def test_valid_moves_seventh_row(self):
+    def test_valid_moves_seventh_row_white(self):
         board = Board()
-        board.set_config(flat="_"*64)
+        board.set_config(flat="_"*64) # it doesn't matter much
         pawn = Pawn(board, square=Square(1, 0), color=conf.WHITE)
         expected = [Square(0, 0)]
         result = pawn.valid_moves()
         self.assertEqual(expected, result)
+       
+    def test_valid_moves_second_row_black(self):
+        board = Board()
+        board.set_config(flat="_"*64) # it doesn't matter much
+        pawn = Pawn(board, square=Square(6, 0), color=conf.BLACK)
+        expected = [Square(7, 0)]
+        result = pawn.valid_moves()
+        self.assertEqual(expected, result)
         
-
-    #@patch('board.Board.notify_move')
-    #def test_move_forward_black(self, notify):
-    #    pawn = Pawn(Board(), square=Square(1, 1), color=conf.BLACK)
-    #    pawn.move_forward()
-    #    expected = Square(2, 1)
-    #    result = pawn.square
-    #    self.assertEqual(expected, result)
-    #    assert notify.called
-
-    #@patch('board.Board.notify_move')
-    #def test_move_forward_white(self, notify):
-    #    pawn = Pawn(Board(), square=Square(6, 3), color=conf.WHITE)
-    #    pawn.move_forward()
-    #    expected = Square(5, 3)
-    #    result = pawn.square
-    #    self.assertEqual(expected, result)
-    #    assert notify.called
-
-
-    #def test_legal_moves(self):
-    #    pawn = Pawn(Board(), square=Square(6, 0), color=conf.WHITE)
-    #    expected = [Square(5, 0), Square(4, 0)]
-    #    result = pawn.legal_moves()
-    #    self.assertEqual(expected, result)
-
 
 if __name__ == '__main__':
     unittest.main()
