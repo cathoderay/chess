ff87432d5496de26b30378b387859c6368643099
Ronald Kaiser <raios.catodicos@gmail.com>
2012-10-07 21:35:11 -0300

    first step in Rook.valid_moves

--- rook.py
+++ rook.py
@@ -1,6 +1,19 @@
+from itertools import takewhile
+
 from piece import Piece
 
 
 class Rook(Piece):
     def __init__(self, board, square, color):
         super(Rook, self).__init__(board, square, color)
+
+    def till_piece(self, func):
+       return [s for s in takewhile(lambda x: self.board.is_empty(x), func())]
+
+    def valid_moves(self):
+        norths = self.till_piece(self.square.norths)
+        souths = self.till_piece(self.square.souths)
+        wests = self.till_piece(self.square.wests)
+        easts = self.till_piece(self.square.easts)
+        moves = norths + souths + wests + easts
+        return moves
--- tests/test_rook.py
+++ tests/test_rook.py
@@ -12,3 +12,15 @@ class RookTest(unittest.TestCase):
     def test_calls_super_in_init(self, init):
         rook = Rook(Board(), Square(4, 2), WHITE)
         assert init.called
+
+    def test_valid_moves_clean_board(self):
+        b = Board()
+        b.clean()
+        rook = Rook(b, Square(4, 2), WHITE)
+        expected = [Square(3, 2), Square(2, 2), Square(1, 2),
+                    Square(0, 2), Square(5, 2), Square(6, 2),
+                    Square(7, 2), Square(4, 1), Square(4, 0),
+                    Square(4, 3), Square(4, 4), Square(4, 5),
+                    Square(4, 6), Square(4, 7)]
+        result = rook.valid_moves()
+        self.assertEqual(expected, result)
