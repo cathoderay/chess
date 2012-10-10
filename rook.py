1ceacf3e6c199078d7327c9302dafe43dca8cfc0
Ronald Kaiser <raios.catodicos@gmail.com>
2012-10-10 16:19:56 -0300

    rook attacking moves

--- rook.py
+++ rook.py
@@ -7,8 +7,26 @@ class Rook(Piece):
     def __init__(self, board, square, color):
         super(Rook, self).__init__(board, square, color)
 
-    def till_piece(self, func):
-       return [s for s in takewhile(lambda x: self.board.is_empty(x), func())]
+    def till_piece(self, orientation_func):
+       return [s for s in takewhile(lambda x: self.board.is_empty(x),
+                                    orientation_func())]
+
+    def attacking_move(self, sense_func):
+        candidates = filter(lambda x: not self.board.is_empty(x), sense_func())
+        if len(candidates) > 0:
+            candidate = candidates[0]
+        else:
+            return None
+        if self.opposite_color() == self.board.color(candidate):
+            return candidate
+        return None
+
+    def attacking_moves(self):
+        return filter(lambda x: x,
+                      [self.attacking_move(self.square.norths),
+                       self.attacking_move(self.square.souths),
+                       self.attacking_move(self.square.wests),
+                       self.attacking_move(self.square.easts)])
 
     def valid_moves(self):
         norths = self.till_piece(self.square.norths)
@@ -16,4 +34,5 @@ class Rook(Piece):
         wests = self.till_piece(self.square.wests)
         easts = self.till_piece(self.square.easts)
         moves = norths + souths + wests + easts
+        moves.extend(self.attacking_moves())
         return moves
--- tests/test_rook.py
+++ tests/test_rook.py
@@ -1,7 +1,7 @@
 from mock import patch
 import unittest
 
-from conf import WHITE
+from conf import WHITE, BLACK
 from square import Square
 from board import Board
 from rook import Rook
@@ -24,3 +24,28 @@ class RookTest(unittest.TestCase):
                     Square(4, 6), Square(4, 7)]
         result = rook.valid_moves()
         self.assertEqual(expected, result)
+
+    def test_attacking_moves(self):
+        b = Board()
+        b.clean()
+        b.matrix[4][7] = "p"
+        b.matrix[1][2] = "P"
+        b.matrix[4][1] = "p"
+        rook = Rook(b, Square(4, 2), BLACK)
+        expected = [Square(4, 1), Square(4, 7)]
+        result = rook.attacking_moves()
+        self.assertEqual(expected, result)
+
+    def test_attacking_being_added_to_valid_moves(self):
+        b = Board()
+        b.clean()
+        b.matrix[3][4] = "P"
+        rook = Rook(b, Square(3, 3), WHITE) 
+        expected = [Square(2, 3), Square(1, 3), Square(0, 3),
+                    Square(4, 3), Square(5, 3), Square(6, 3),
+                    Square(7, 3), Square(3, 2), Square(3, 1),
+                    Square(3, 0), Square(3, 4)]
+        result = rook.valid_moves()
+        self.assertEqual(expected, result)
+
+
